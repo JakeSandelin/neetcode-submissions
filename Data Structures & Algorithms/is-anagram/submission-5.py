7c96ccf232b94ec9e6f98d6297029ea4949201cb
Jake Sandelin <76856942+JakeSandelin@users.noreply.github.com>
2026-08-22 15:43:31 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s2 = [0]*32
+        t2 = [0]*32
+
+        for i in range(len(s)):
+            s2[ord(s[i])-ord('a')]+= 1
+            t2[ord(t[i])-ord('a')]+= 1
+
+        return s2 == t2
